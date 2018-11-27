14133c2da2b1d8ddef0e33dbde111344b57d72d7
冯玉敏 <fengyumin@cncic.chemchina.com>
2018-11-27 16:33:23 +0800

    first init

--- platforms/ios/WeexEros/WeexEros/AppDelegate.h
+++ platforms/ios/WeexEros/WeexEros/AppDelegate.h
@@ -3,11 +3,11 @@
 //  WeexEros
 //
 //  Created by XHY on 2017/8/7.
-//  Copyright © 2017年 Byte Master. All rights reserved.
+//  Copyright © 2017年 benmu. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <ErosPluginBaseLibrary/BMAppDelegate.h>
+#import <BMBaseLibrary/BMAppDelegate.h>
 
 @interface AppDelegate : BMAppDelegate
 
